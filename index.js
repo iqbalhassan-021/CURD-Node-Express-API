fafc658eef3d5b20011d19789018ff5bad8311df
Iqbal Hassan <102966946+iqbalhassan-021@users.noreply.github.com>
2024-07-13 22:14:07 +0500

    new functios are added

--- index.js
+++ index.js
@@ -10,18 +10,17 @@ const users = require("./Users.json");
 
 
 // Middleware to parse JSON bodies
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//Routs
+//Routes
 //Home
 app.get('/', (req, res) => {
-  res.send('This is a test API!, Use /api/users to load the users');
+  res.send('This is a test API! Use /api/users to load the users');
 });
 
 //Get all the users
 app.get('/api/users', (req, res) => {
-
   return res.json(users);
 });
 
@@ -33,23 +32,52 @@ app.get('/api/users/:id', (req, res) => {
 });
 
 //Edit the user with :id
-app.patch('/api/users/:id',(req, res) => {
-  return res.json({status : "Pending"});
+app.patch('/api/users/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const updatedData = req.body;
+  let user = users.find(user => user.id === id);
+
+  if (user) {
+    Object.assign(user, updatedData);
+    fs.writeFile("./Users.json", JSON.stringify(users), (err) => {
+      if (err) {
+        return res.status(500).json({ status: "Error", message: "Failed to update user" });
+      }
+      return res.json({ status: "Success", user });
+    });
+  } else {
+    return res.status(404).json({ status: "Error", message: "User not found" });
+  }
 });
 
 //Delete the user with :id
-app.delete('/api/users/:id',(req, res) => {
-  return res.json({status : "Pending"});
+app.delete('/api/users/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const userIndex = users.findIndex(user => user.id === id);
+
+  if (userIndex !== -1) {
+    users.splice(userIndex, 1);
+    fs.writeFile("./Users.json", JSON.stringify(users), (err) => {
+      if (err) {
+        return res.status(500).json({ status: "Error", message: "Failed to delete user" });
+      }
+      return res.json({ status: "Success", message: "User deleted" });
+    });
+  } else {
+    return res.status(404).json({ status: "Error", message: "User not found" });
+  }
 });
 
 //Upload a user
-app.post('/api/users',(req, res) => {
+app.post('/api/users', (req, res) => {
   const body = req.body;
-  users.push({...body, id: users.length + 1});
-  fs.writeFile("./Users.json", JSON.stringify(users), (err, data) => {
-    return res.json({status : "Success", id: users.length + 1});
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./Users.json", JSON.stringify(users), (err) => {
+    if (err) {
+      return res.status(500).json({ status: "Error", message: "Failed to add user" });
+    }
+    return res.json({ status: "Success", id: users.length });
   });
-  
 });
 
 // Start the server
